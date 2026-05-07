4701434c0f012d83bcaf6c1af1d546198d6b978d
MiguelT-R-M <miguelhrs123@gmail.com>
2026-05-07 15:18:24 -0300

    ...

--- teste_de_ula/testbench.v
+++ teste_de_ula/testbench.v
@@ -1,9 +1,9 @@
 `timescale 1ns/1ps
 
 module ula_tb;
-    reg [3:0] A, B;
+    reg signed [4:0] A, B;
     reg [2:0] Seletor;
-    wire [3:0] result;
+    wire signed [5:0] result;
     wire Zero;
 
     ula dut(.A(A), .B(B), .Seletor(Seletor), .result(result), .Zero(Zero));
@@ -16,11 +16,11 @@ module ula_tb;
         $dumpvars(0, ula_tb);
 
         // Test Case 1: AND operation (3'b000)
-        A = 4'b1101; B = 4'b1011; Seletor = 3'b000;
+        A = {3'b001, 2'b11}; B = 5'b00111; Seletor = 3'b000;
         #10; // Wait 10 time units
 
         // Test Case 2: Default/Zero check
-        A = 4'b1101; B = 4'b0000; Seletor = 3'b001;
+        A = 5'b00101; B = 5'b10001; Seletor = 3'b001;
         #10;
 
         $finish; // End simulation
--- teste_de_ula/teste.v
+++ teste_de_ula/teste.v
@@ -1,13 +1,25 @@
-module ula(input wire[3:0] A, input wire[3:0] B, input wire[2:0] Seletor, output reg[3:0] result, output reg Zero, inout wire[3:0] useless);
+module ula(A, B, Seletor, result, Zero, Dontcare);
+
+    input wire signed [4:0] A, B;
+    input wire[2:0] Seletor;
+    output reg signed [5:0] result;
+    output reg Zero;
+    inout wire[3:0] Dontcare;
 
     always @(*) begin
         case (Seletor)
             
-            3'b000: result = A & B;
-            3'b001: result = A | B;
+            3'b000: result = A + B;
+            3'b001: result = A - B;
+            3'b010: result = ~B + 1;
+            3'b011: result = (A==B)? 6'b000001: 6'b000000;
+            3'b100: result = (A>B)? 6'b000001: 6'b000000;
+            3'b101: result = (A<B)? 6'b000001: 6'b000000;
+            3'b110: result = A & B;
+            3'b111: result = A ^ B;
 
-            default: result = 4'b0000;
+            default: result = 6'bxxxxxx;
         endcase
-        Zero = (result == 4'b0000);
+        Zero = (result == 6'b000000);
     end
 endmodule
